3bf4eb4660cdd0f230fee9868e305bfa6b07f757
stefanoyoyo <git.stefano@outlook.it>
2022-02-11 10:52:36 +0100

    aggiunto il metodo per la generazione dell'uuuid

--- src/app/change-field-value/change-field-value.component.ts
+++ src/app/change-field-value/change-field-value.component.ts
@@ -43,7 +43,8 @@ export class ChangeFieldValueComponent implements OnInit {
       li sostituisco.
      */
     const numOccurrences = -1;
-    const uuuids: string[] =  this.getSubstituteValues(numOccurrences) 
+    const uuuids: string[] =  this.getSubstituteValues(numOccurrences);
+    console.log(uuuids) 
     const output = this.replaceValues(uuuids);
     return output;
   }
@@ -65,9 +66,30 @@ export class ChangeFieldValueComponent implements OnInit {
   }
 
   /**making an array of uuuid given having the same specified length */
-  getUUUids(numOccurrences: any): any[] {
-    throw new Error('Method not implemented.');
+  getUUUids(numOccurrences: number): any[] {
+    const uuuids: string[] = [];
+    for(let i = 0; i < numOccurrences; i++) {
+      uuuids.push(this.generateUUID());
+    }
+    return uuuids;
   }
+
+
+  generateUUID() { // Public Domain/MIT
+    var d = new Date().getTime();//Timestamp
+    var d2 = ((typeof performance !== 'undefined') && performance.now && (performance.now()*1000)) || 0;//Time in microseconds since page-load or 0 if unsupported
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16;//random number between 0 and 16
+        if(d > 0){//Use timestamp until depleted
+            r = (d + r)%16 | 0;
+            d = Math.floor(d/16);
+        } else {//Use microseconds since page-load if supported
+            r = (d2 + r)%16 | 0;
+            d2 = Math.floor(d2/16);
+        }
+        return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+}
 
   // #endregion
 

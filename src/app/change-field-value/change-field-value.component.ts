d769b0a7c2fe72a882295671e7d51be667897466
stefanoyoyo <git.stefano@outlook.it>
2022-02-11 10:43:58 +0100

    lasciato le basi per sostituire i vecchi valori con dei nuovi in change-field-value component

--- src/app/change-field-value/change-field-value.component.ts
+++ src/app/change-field-value/change-field-value.component.ts
@@ -11,6 +11,14 @@ export class ChangeFieldValueComponent implements OnInit {
 
   pathToDelete = '';
   changeToApply = '';
+  selectedValue = -1;
+
+  options = [
+    '---No value---',
+    'uuuId'
+  ]
+
+  output = '';
 
   constructor() { }
 
@@ -18,6 +26,49 @@ export class ChangeFieldValueComponent implements OnInit {
   }
 
   onKeyUp() {
-
   }
+
+  onSelectChange() {
+    if (this.selectedValue === 0) { return; }
+    this.changeToApply =  `***COMMAND_${this.options[this.selectedValue]}***`;
+    const output = this.writeAllValues();
+  }
+
+  // #region uuuids
+
+  writeAllValues() {
+    /*
+      conto nella stringa il numero di valori da sostituire.
+      genero lo stesos numero di valori che li andranno a sostituire.
+      li sostituisco.
+     */
+    const numOccurrences = -1;
+    const uuuids: string[] =  this.getSubstituteValues(numOccurrences) 
+    const output = this.replaceValues(uuuids);
+    return output;
+  }
+
+  /**Rimpiazzo i valori vecchi con quelli specificati come parametro  */
+  replaceValues(uuuids: string[]) {
+    throw new Error('Method not implemented.');
+  }
+
+  /**Ottengo i valori da rimpiazzare al posto dei valori vecchi */
+  getSubstituteValues(numOccurrences) {
+    let values = [];
+    switch(this.selectedValue) {
+      case 1: 
+      values  = this.getUUUids(numOccurrences);
+      break;
+    }
+    return values;
+  }
+
+  /**making an array of uuuid given having the same specified length */
+  getUUUids(numOccurrences: any): any[] {
+    throw new Error('Method not implemented.');
+  }
+
+  // #endregion
+
 }

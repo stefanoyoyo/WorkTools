84b3bf06912318717343fb5c282ecdc00ef7cb14
stefanoyoyo <git.stefano@outlook.it>
2022-02-13 00:01:06 +0100

    bug fix

--- src/app/change-field-value/change-field-value.component.ts
+++ src/app/change-field-value/change-field-value.component.ts
@@ -4,35 +4,32 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-change-field-value',
   templateUrl: './change-field-value.component.html',
-  styleUrls: ['./change-field-value.component.css']
+  styleUrls: ['./change-field-value.component.css'],
 })
 export class ChangeFieldValueComponent implements OnInit {
-
   @Input() input = '';
 
   fieldToChange = '';
   changeToApply = '';
   selectedValue = -1;
 
-  options = [
-    '---No value---',
-    'uuuId'
-  ]
+  options = ['---No value---', 'uuuId'];
 
   output = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
-  onKeyUp() {
-  }
+  onKeyUp() {}
 
   onSelectChange() {
-    if (this.selectedValue === 0) { return; }
-    this.changeToApply =  `***COMMAND_${this.options[this.selectedValue]}***`;
+    if (this.selectedValue === 0) {
+      return;
+    }
+    this.changeToApply = `***COMMAND_${this.options[this.selectedValue]}***`;
     const output = this.writeAllValues();
+    this.output = output;
   }
 
   // #region uuuids
@@ -42,9 +39,12 @@ export class ChangeFieldValueComponent implements OnInit {
     genero lo stesos numero di valori che li andranno a sostituire.
     li sostituisco.
   */
-  writeAllValues() {
-    const numOccurrences = this.substringOccurrencesNumber(this.input, this.fieldToChange);
-    const uuuids: string[] =  this.getSubstituteValues(numOccurrences);
+  writeAllValues(): string {
+    const numOccurrences = this.substringOccurrencesNumber(
+      this.input,
+      this.fieldToChange
+    );
+    const uuuids: string[] = this.getSubstituteValues(numOccurrences);
     const output = this.replaceValues(this.input, this.fieldToChange, uuuids);
     return output;
   }
@@ -52,62 +52,61 @@ export class ChangeFieldValueComponent implements OnInit {
   // #region replace values of a json string of a specified field name
 
   /**Method replacing the old field values of the specified json string
-   *  with the new specified values. 
-   * This method's working is based on the fact that 
-   * JavaScript's replace method only substitutes the 
-   * first occurence. Getting an array of the values that 
-   * must be substitued by parsing the input string, is 
-   * possibile to apply a sostitution for each of those 
+   *  with the new specified values.
+   * This method's working is based on the fact that
+   * JavaScript's replace method only substitutes the
+   * first occurence. Getting an array of the values that
+   * must be substitued by parsing the input string, is
+   * possibile to apply a sostitution for each of those
    * words with any other word.
    * @param input input json string containing the valeus to replace at the specified field name
    * @param fieldToChange name of the field to change
    * @param uuuids string array containg the values which will be placed instead of the old values
-  */
+   */
   replaceValues(input: string, fieldToChange: string, uuuids: string[]) {
     const toSubstitue: string[] = this.getTobeSubstitued(input, fieldToChange);
     toSubstitue.forEach((sobstitute, index) => {
-      this.input = this.input.replace(sobstitute, uuuids[index])
+      input = input.replace(sobstitute, uuuids[index]);
     });
+    return input;
   }
 
-  
   // #region get values to be substituted
 
-  /**Method getting all the values that will be replaced into the string 
+  /**Method getting all the values that will be replaced into the string
    * @param input input json string containing the valeus to replace at the specified field name
    * @param fieldToChange name of the field to change
-   * EXAMPLE: 
+   * EXAMPLE:
    * input = {"id": "ciao", "bla" : {  "id" : "33"}}
    * fieldToChange = "id"
    * var result = ["ciao", "33"]
-  */
+   */
   getTobeSubstitued(input: string, fieldToChange: string): string[] {
-    let index = -1; 
+    let index = -1;
     const toBeSubstituted: string[] = [];
     const substrCount = this.substringOccurrencesNumber(input, fieldToChange);
-    for(let i = 0; i<substrCount; i++) {
-      index = i+1;
+    for (let i = 0; i < substrCount; i++) {
+      index = i + 1;
       const fieldIndex = this.getSubstringPosition(input, fieldToChange, index);
       const wordApexes: string = this.getObjByIndex(input, fieldIndex);
-      const word = wordApexes.substring(1, wordApexes.length-1);
+      const word = wordApexes.substring(1, wordApexes.length - 1);
       toBeSubstituted.push(word);
     }
 
     return toBeSubstituted;
   }
-  
-  /**Getting the field value given the first '"' field's char.AGGIUNGERE AGLI HELPERS 
+
+  /**Getting the field value given the first '"' field's char.AGGIUNGERE AGLI HELPERS
    * @param input input json string containing the value of the field name to get
    * @param fieldIndex first '"" character index of the field having the value to get
-   * EXAMPLE: 
+   * EXAMPLE:
    * input = {"id": "ciao", "bla" : {  "id" : "33"}}
    * fieldIndex = 2 (ie, '"' in "id")
    * var result = "ciao"
-  */
+   */
   getObjByIndex(input: string, fieldIndex: number): string {
-
     // moving out of the fiels index of '"'
-    fieldIndex+= `"${this.fieldToChange}"`.length;
+    fieldIndex += `"${this.fieldToChange}"`.length;
 
     // looking for the initial index
     while (input[fieldIndex] !== '"') {
@@ -116,23 +115,23 @@ export class ChangeFieldValueComponent implements OnInit {
     const start = fieldIndex;
 
     // Moving to the index next to '"'
-    fieldIndex+= 1;
+    fieldIndex += 1;
 
     // looking for the final index
     while (input[fieldIndex] !== '"') {
       ++fieldIndex;
     }
 
-    const end = fieldIndex+1;
+    const end = fieldIndex + 1;
 
     // getting the word between initial and final indexes
     const word = this.getWordByIndexes(this.input, start, end);
     return word;
   }
-  
+
   /**Method getting the subword included between the given indexes AGGIUNGERE AGLI HELPERS */
-  getWordByIndexes(input: string,start: number,end: number) {
-    return input.substring(start, end)
+  getWordByIndexes(input: string, start: number, end: number) {
+    return input.substring(start, end);
   }
 
   // #endregion
@@ -147,49 +146,57 @@ export class ChangeFieldValueComponent implements OnInit {
     /* SENZA QUESTO SWITCH NON POSSO PERMETTERE AL COMPONENTE DI 
        SOSTITUIRE IN PIU' MODI IL VALORE  */
     // switch(this.selectedValue) {
-    //   case 1: 
-    //     console.log('fire') // NON entro qui! 
+    //   case 1:
+    //     console.log('fire') // NON entro qui!
     //     values  = this.getUUUids(numOccurrences);
     //     break;
     // }
 
-    values  = this.getUUUids(numOccurrences);
+    values = this.getUUUids(numOccurrences);
 
     return values;
   }
 
-
   /**making an array of uuuid given having the same specified length */
   getUUUids(numOccurrences: number): any[] {
     const uuuids: string[] = [];
-    for(let i = 0; i < numOccurrences; i++) {
+    for (let i = 0; i < numOccurrences; i++) {
       uuuids.push(this.generateUUID());
     }
     return uuuids;
   }
 
-
-  generateUUID() { // Public Domain/MIT
-    var d = new Date().getTime();//Timestamp
-    var d2 = ((typeof performance !== 'undefined') && performance.now && (performance.now()*1000)) || 0;//Time in microseconds since page-load or 0 if unsupported
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        var r = Math.random() * 16;//random number between 0 and 16
-        if(d > 0){//Use timestamp until depleted
-            r = (d + r)%16 | 0;
-            d = Math.floor(d/16);
-        } else {//Use microseconds since page-load if supported
-            r = (d2 + r)%16 | 0;
-            d2 = Math.floor(d2/16);
+  generateUUID() {
+    // Public Domain/MIT
+    var d = new Date().getTime(); //Timestamp
+    var d2 =
+      (typeof performance !== 'undefined' &&
+        performance.now &&
+        performance.now() * 1000) ||
+      0; //Time in microseconds since page-load or 0 if unsupported
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(
+      /[xy]/g,
+      function (c) {
+        var r = Math.random() * 16; //random number between 0 and 16
+        if (d > 0) {
+          //Use timestamp until depleted
+          r = (d + r) % 16 | 0;
+          d = Math.floor(d / 16);
+        } else {
+          //Use microseconds since page-load if supported
+          r = (d2 + r) % 16 | 0;
+          d2 = Math.floor(d2 / 16);
         }
-        return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
-    });
-}
+        return (c === 'x' ? r : (r & 0x3) | 0x8).toString(16);
+      }
+    );
+  }
 
   // #endregion
 
   // #region string ɵHttpInterceptingHandler
 
-    /**Metodo che ritorna il numero di volte che una sottostringa è contneuta in una stringa 
+  /**Metodo che ritorna il numero di volte che una sottostringa è contneuta in una stringa
    * @param word contente le sottostringhe da contare
    * @param subword sottostringa da contare
    */
@@ -200,19 +207,18 @@ export class ChangeFieldValueComponent implements OnInit {
     return num;
   }
 
-      /**Metodo che permette di ottenere l'indice in cui appare l'occorrenza ennesima della sottostringa nella stirnga. 
-       * NOTA: l'indice deve partire SEMPRE da 1! 
-     * ESEMPIO: dato
-     * "ab ac ab ad ab ae ab af"
-     *  • la prima occorrenza di ab si trova all'indice 0
-     *  • la seconda occorrenza di ab si trova all'indice 3
-     *  • la terza occorrenza di ab si trova all'indice 5
-     *  • ecc..
-     */
-    getSubstringPosition(bbcode, subString, index): number {
-      return bbcode.split(subString, index).join(subString).length;
-    }
+  /**Metodo che permette di ottenere l'indice in cui appare l'occorrenza ennesima della sottostringa nella stirnga.
+   * NOTA: l'indice deve partire SEMPRE da 1!
+   * ESEMPIO: dato
+   * "ab ac ab ad ab ae ab af"
+   *  • la prima occorrenza di ab si trova all'indice 0
+   *  • la seconda occorrenza di ab si trova all'indice 3
+   *  • la terza occorrenza di ab si trova all'indice 5
+   *  • ecc..
+   */
+  getSubstringPosition(bbcode, subString, index): number {
+    return bbcode.split(subString, index).join(subString).length;
+  }
 
   // #endregion
-
 }

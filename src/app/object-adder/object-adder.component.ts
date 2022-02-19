771ad7ed825a64f28d3d1d53aa4361b7e2775a2f
stefanoyoyo <git.stefano@outlook.it>
2022-02-19 10:31:14 +0100

    modifiche grafice

--- src/app/object-adder/object-adder.component.ts
+++ src/app/object-adder/object-adder.component.ts
@@ -10,12 +10,27 @@ export class ObjectAdderComponent implements OnInit {
   @Input() input;
 
   pathToEdit = './';
-  
+
   fieldValues = '';
+
+  result = '';
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onKeyUp() {
+    const input = JSON.parse(this.input);
+    const isArray = Array.isArray(input);
+    let result = null; 
+
+    if (isArray) {
+      result = null;
+    } else {
+      result = null;
+    }
+
   }
 
 }

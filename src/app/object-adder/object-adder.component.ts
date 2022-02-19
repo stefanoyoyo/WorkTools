8b7a04bd0a49573bc62e10abb693fda685f14c88
stefanoyoyo <git.stefano@outlook.it>
2022-02-19 11:29:24 +0100

    object add component implemented add from specified array values

--- src/app/object-adder/object-adder.component.ts
+++ src/app/object-adder/object-adder.component.ts
@@ -31,6 +31,32 @@ export class ObjectAdderComponent implements OnInit {
       result = null;
     }
 
+    const nameToAdd = this.pathToEdit.replace('./', '') 
+    let inputValues = JSON.parse(this.input);
+    const fieldValues = JSON.parse(this.fieldValues);
+    const added = this.addValues(nameToAdd, inputValues, fieldValues );
+    console.log('added')
+    console.log(added)
+
   }
+
+/**Method adding the specified values to the output json */
+addValues(nameToAdd: string, inputValues: object[], fieldValues: object[]){
+  if (!this.isValuesArray()) { return null; }
+  console.log(fieldValues)
+  for(let i = 0; i < inputValues.length; i++) {
+    const value = fieldValues[i];
+    inputValues[i][nameToAdd] = i <= fieldValues.length ? value : '';  
+  }
+
+  return inputValues;
+}
+
+isValuesArray() {
+  if (this.fieldValues == null) { return false; }
+  if (this.fieldValues.charAt(0) !== '[') { return false; }
+  const pseudoarr = JSON.parse(this.fieldValues);
+  return pseudoarr != null;
+}
 
 }

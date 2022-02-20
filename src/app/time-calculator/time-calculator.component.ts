3172f590b64780baf75bdf6d0eab7e9624bc98ac
stefanoyoyo <git.stefano@outlook.it>
2022-02-20 11:36:12 +0100

    added time calculator html

--- src/app/time-calculator/time-calculator.component.ts
+++ src/app/time-calculator/time-calculator.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TimeCalculatorComponent implements OnInit {
 
+  left = '';
+  right = '';
+
+  result = '';
+
   constructor() { }
 
   ngOnInit() {
